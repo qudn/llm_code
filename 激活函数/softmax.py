419790ba9a7153bdaee4fe4a07722da78eb849fd
J R Douceur <115005855+qudn@users.noreply.github.com>
2026-04-12 21:47:32 +0800

    Update softmax.py

--- 激活函数/softmax.py
+++ 激活函数/softmax.py
@@ -3,5 +3,5 @@ import numpy as np
 def softmax(x):
   x = np.asarray(x, dtype = np.float64)
   x_max = np.max(x, axis = -1, keepdim = True)
-  e_x = np.esp(x - x_max)
+  e_x = np.exp(x - x_max)
   return e_x / np.sum(x, axis = -1, keepdim = True)
